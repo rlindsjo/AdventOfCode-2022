876f124ad2949d4c26c1aa30953ff9c4d1c14809
Roger Lindsjö <roger@tilialacus.net>
2022-12-25 16:24:43 +0100

    Rename forgotten method

--- src/test/java/net/tilialacus/adventofcode2022/PipesTest.java
+++ src/test/java/net/tilialacus/adventofcode2022/PipesTest.java
@@ -2,15 +2,12 @@ package net.tilialacus.adventofcode2022;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
-import static java.util.Collections.emptySet;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PipesTest {
 
     @Test
-    void name() {
+    void singleMover() {
         Pipes pipes = new Pipes();
         FileUtil.resourceLinesAsList("pipes_test.txt").forEach(pipes::add);
         Pipes.Move move = pipes.getBest(30);
