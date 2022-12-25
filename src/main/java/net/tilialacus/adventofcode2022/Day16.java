30c69011acc7e9b99673e81622bcc8269dcd4977
Roger Lindsjö <roger@tilialacus.net>
2022-12-25 16:12:31 +0100

    Spelling

--- src/main/java/net/tilialacus/adventofcode2022/Day16.java
+++ src/main/java/net/tilialacus/adventofcode2022/Day16.java
@@ -6,6 +6,6 @@ public class Day16 {
         Pipes pipes = new Pipes();
         FileUtil.resourceLinesAsList("day16/pipes.txt").forEach(pipes::add);
         Pipes.Move move = pipes.getBest(30);
-        System.err.println("Macimum released " + move.score);
+        System.err.println("Maximum released " + move.score);
     }
 }
